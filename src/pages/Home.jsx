1d2c9288bf50640404a20464aabfb36e4d2d7a8b
Laura Ortiz <bjtamyl@hotmail.com>
2022-10-04 17:27:43 -0300

    seccion about en progreso

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Slider from "../components/Slider";
+import Presentacion from "../components/Presentacion";
 
 const Home = () => {
   return (
     <>
       <Slider />
+      <br />
+      <Presentacion />
+      <br />
     </>
   );
 };

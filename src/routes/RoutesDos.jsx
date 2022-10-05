6f11aba44a9a8aeff9b2ca8da38b7c47ebacb793
DamiAbi <damithapam@gmail.com>
2022-10-04 23:16:58 -0300

    galeria en progreso

--- src/routes/RoutesDos.jsx
+++ src/routes/RoutesDos.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Home from "../pages/Home";
-import Galeria from "../pages/Galeria";
+
 import Experiencias from "../pages/Experiencias";
 import Paquetes from "../pages/Paquetes";
 import SobreMi from "../pages/SobreMi";
 import PreguntasFrecuentes from "../pages/PreguntasFrecuentes";
 import NotFound from "../pages/NotFound";
+import FinalBoard from "../pages/FinalBoard";
 
 const RoutesDos = () => {
   return (
@@ -15,7 +16,8 @@ const RoutesDos = () => {
       <NavBar />
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/galeria" element={<Galeria />} />
+        <Route path="/galeria" element={<FinalBoard
+         />} />
         <Route path="/experiencias" element={<Experiencias />} />
         <Route path="/paquetes" element={<Paquetes />} />
         <Route path="/sobremi" element={<SobreMi />} />

f57ca9dc2d1f03834f9801eeaeba6413fa6dbff0
DamiAbi <damithapam@gmail.com>
2022-10-06 22:19:01 -0300

    contacto vacio

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,121 +1,62 @@
-import React, { useState } from "react";
-import contacto from "../css/contacto.css";
-import emailjs from "@emailjs/browser";
+import React, { useRef } from "react";
+import emailjs, { init } from "@emailjs/browser";
 
 function Contacto() {
-  const [to_name, setTo_name] = useState("");
-  const [from_name, setFrom_name] = useState("");
-  const [message, setMessage] = useState("");
+  init("M6JhJZxCClH7lHq8_1RPt");
+  const form = useRef();
 
-  const submitInfo = (event) => {
-    event.preventDefault(event);
-
-    console.log(to_name + from_name + message);
-
-    const emailContent = {
-      to_name: to_name,
-      from_name: from_name,
-      message: message,
-    };
-
-    emailjs
-      .send(
-        "service_w5b2q8p",
-        "template_p4th3sl",
-        emailContent,
-        "VZCDTgZoOTA3lePFn"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm("service_0u60yz7", "template_9nch3xa", form.current, "gFG8zgB0wykPF7gqP").then(
+      (result) => {
+        alert("Message Sent Successfully");
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
   };
   return (
-    <div className="contact container py-5">
-      <div className="row">
-        <div className="col-lg-6 mx-auto">
-          <div className="card">
-            <div className="card-body">
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="head text-center text-white py-3">
-                    <h3>Contactanos</h3>
-                  </div>
-                </div>
-              </div>
-              <form className="form py-3">
-                <div className="form-row my-4">
-                  <div className="col-lg-12">
-                    <input
-                      type="text"
-                      name="to_name"
-                      tabIndex="1"
-                      className="effect-1"
-                      placeholder="Ingrese su nombre"
-                      onChange={(event) => {
-                        setTo_name(event.target.value);
-                      }}
-                      required
-                      autoFocus
-                    />
-                    <span className="Focus-border"></span>
-                  </div>
-                </div>
-                <div className="form-row pb-4 ">
-                  <div className="col-lg-12 ">
-                    <input
-                      type="email"
-                      name="from_name"
-                      tabIndex="2"
-                      className="effect-1 "
-                      placeholder="Ingrese su correo electrónico"
-                      onChange={(event) => {
-                        setFrom_name(event.target.value);
-                      }}
-                      required
-                    ></input>
-                    <span className="Focus-border"></span>
-                  </div>
-                </div>
-                <div className="form-row pt-2">
-                  <div className="col-lg-12">
-                    <textarea
-                      type="text"
-                      tabIndex="3"
-                      name="message"
-                      className="effect-1 "
-                      placeholder="Ingrese su mensaje"
-                      onChange={(event) => {
-                        setMessage(event.target.value);
-                      }}
-                      required
-                    ></textarea>
-                    <span className="Focus-border"></span>;
-                  </div>
-                </div>
-                <div className="form-row pt-4 ">
-                  <div className="offset-4 col-lg-12">
-                    <p>
-                      <input type="checkbox" />
-                      No soy un robot
-                    </p>
-                  </div>
-                  <div className="offset-4 col-lg-12">
-                    <button className="btn1" type="submit" onClick={submitInfo}>
-                      Enviar
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
+    <div className="container">
+      <form onSubmit={handleSubmit} ref={form}>
+        <h1 className="text-center">Registration Form</h1>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label htmlFor="First Name">First Name</label>
+            <input type="text" className="form-control" name="firstname" />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="Last Name">Last Name</label>
+            <input type="text" className="form-control" name="lastname" />
+          </div>
+          <div className="form-group col-12">
+            <label htmlFor="inputAddress">Address</label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputAddress"
+              placeholder="1234 Main St"
+              name="user_address"
+            />
+          </div>{" "}
+          <div className="form-group col-md-6">
+            <label htmlFor="message">message</label>
+            <textarea
+              type="text"
+              className="form-control"
+              id="inputmessage4"
+              name="user_message"
+            />
           </div>
         </div>
-      </div>
+
+        <button type="submit" className="btn btn-primary">
+          Sign in
+        </button>
+      </form>
     </div>
   );
 }
+
 export default Contacto;

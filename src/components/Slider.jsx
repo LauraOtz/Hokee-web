8b377aba7ea7492d42158c34396b8403e66be495
Laura Ortiz <bjtamyl@hotmail.com>
2022-10-07 17:19:46 -0300

    cambio diseño de boton slider, en proceso

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -9,54 +9,105 @@ import slider from "../css/slider.css";
 const Slider = () => {
   return (
     <div
-      id="carouselExampleControls"
-      class="carousel slide "
+      id="carouselExampleCaptions"
+      className="carousel slide"
       data-bs-ride="carousel"
     >
-      <div class="carousel-inner">
-        {/* <div class="carousel-item active " data-bs-interval="3000">
-          <img src={slider1} class="d-block w-100" alt="..." />
-        </div> */}
-        <div class="carousel-item active" data-bs-interval="3000">
-          <img src={slider2} class="d-block w-100" alt="..." />
-          <div class="carousel-caption d-none d-md-block">
-            <button className="btn btn2">
-              <h3>Reserva tu sesión ahora</h3>
-            </button>
+      {/* <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div> */}
+      <div className="carousel-inner">
+        <div className="carousel-item active" data-bs-interval="3000">
+          <img src={slider2} className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <div>
+              <h1>FOTOGRAFIA DE MASCOTAS</h1>
+            </div>
+            <div>
+              <button className="btn btn2">
+                <h2>Reserva tu sesión ahora</h2>
+              </button>
+            </div>
+            <div>
+              <button className="btn btn3">
+                <h3>Quiero saber más</h3>
+              </button>
+            </div>
           </div>
-          <div class="carousel-caption d-none d-md-block">
-            <button className="btn btn3 col-lg-6">
-              <h3>Quiero saber más</h3>
-            </button>
+        </div>
+        <div className="carousel-item" data-bs-interval="3000">
+          <img src={slider3} className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <div>
+              <h1>FOTOGRAFIA DE MASCOTAS</h1>
+            </div>
+            <div>
+              <button className="btn btn2">
+                <h2>Reserva tu sesión ahora</h2>
+              </button>
+            </div>
+            <div>
+              <button className="btn btn3">
+                <h3>Quiero saber más</h3>
+              </button>
+            </div>
           </div>
         </div>
-        <div class="carousel-item" data-bs-interval="3000">
-          <img src={slider3} class="d-block w-100" alt="..." />
-        </div>
-        <div class="carousel-item" data-bs-interval="3000">
-          <img src={slider4} class="d-block w-100" alt="..." />
-        </div>
-        <div class="carousel-item" data-bs-interval="3000">
-          <img src={slider5} class="d-block w-100" alt="..." />
+        <div className="carousel-item" data-bs-interval="3000">
+          <img src={slider4} className="d-block w-100" alt="..." />
+          <div className="carousel-caption d-none d-md-block">
+            <div>
+              <h1>FOTOGRAFIA DE MASCOTAS</h1>
+            </div>
+            <div>
+              <button className="btn btn2">
+                <h2>Reserva tu sesión ahora</h2>
+              </button>
+            </div>
+            <div>
+              <button className="btn btn3">
+                <h3>Quiero saber más</h3>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
       <button
-        class="carousel-control-prev"
+        className="carousel-control-prev"
         type="button"
-        data-bs-target="#carouselExampleControls"
+        data-bs-target="#carouselExampleCaptions"
         data-bs-slide="prev"
       >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
       </button>
       <button
-        class="carousel-control-next"
+        className="carousel-control-next"
         type="button"
-        data-bs-target="#carouselExampleControls"
+        data-bs-target="#carouselExampleCaptions"
         data-bs-slide="next"
       >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
       </button>
     </div>
   );

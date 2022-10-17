e8904e84134ece4bcfc28799ab8c5b31c1af37f3
Laura Ortiz <bjtamyl@hotmail.com>
2022-10-17 14:14:51 -0300

    cards de experiencias listas

--- src/components/Experiencias.jsx
+++ src/components/Experiencias.jsx
@@ -1,68 +1,89 @@
 import React from "react";
-import slider1 from "../assets/slider1.jpg";
-import slider3 from "../assets/slider3.jpg";
-import  slider2  from "../assets/slider2.jpg";
+import experiencias from "../css/experiencias.css";
+import gini from "../assets/gini.jpg";
+import huma1 from "../assets/huma1.jpg";
+import otti from "../assets/otti.jpg";
 
-import "../css/PreguntasFrecuentes.css"
+import "../css/PreguntasFrecuentes.css";
 
 const PreguntasFrecuentes = () => {
-
-
   return (
-    
     <>
-    <h1 className="Titulop"> Experiencias</h1>
- 
-    <div className="cardE mb-3" >
-    <div className="row g-0">
-      <div className="col-md-4">
-        <img src={slider1} className="img-fluid rounded-start" alt="..."/>
-      </div>
-      <div className="col-md-8">
-        <div className="card-body">
-          <h5 className="card-title"><strong> Card title</strong></h5>
-          <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer This is a wider card with supporting text below as a natural lead-in to additional content.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer This content is a little bit longer.</p>
-         
-        </div>
-      </div>
-    </div>
-  </div>
-  <hr className="hrp" />
+      <h1 className="Titulop"> Experiencias (frase emotiva)</h1>
 
-  <div className="cardE mb-3" >
-  <div className="row g-0">
-    <div className="col-md-4">
-      <img src={slider2} className="img-fluid rounded-start" alt="..."/>
-    </div>
-    <div className="col-md-8">
-      <div className="card-body">
-        <h5 className="card-title"> <strong> Card title</strong></h5>
-        <p className="card-text">This is a wider cardThis is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer  with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-       
-      </div>
-    </div>
-  </div>
-</div>
-<hr className="hrp" />
-<div className="cardE mb-3" >
-    <div className="row g-0">
-      <div className="col-md-4">
-        <img src={slider3} className="img-fluid rounded-start" alt="..."/>
-      </div>
-      <div className="col-md-8">
-        <div className="card-body">
-          <h5 className="card-title"><strong> Card title</strong></h5>
-          <p className="card-text">This is a wider card with supporting text below as a natural lead-in to This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer additional content. This content is a little bit longer.</p>
-       
+      <div className="cardE mb-3">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src={gini} className="img-fluid rounded-start" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body cardEx">
+              <h5 className="card-title mb-3 ">
+                <strong> Gini Rohmer Litzmann</strong>
+              </h5>
+              <p className="card-text textE text-dark">
+                “Tremendas las fotos!!!! Estoy enamorada!!! Gracias! Hace años
+                quería sacarles fotos así a mis pequeños, gracias por cumplir mi
+                sueño!"
+              </p>
+              <span className="text-dark">
+                San Miguel de Tucumán - Argentina - 2020
+              </span>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  <hr className="hrp" />
-  
+      <hr className="hrp" />
+
+      <div className="cardE mb-3">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src={huma1} className="img-fluid rounded-start" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body cardEx ">
+              <h5 className="card-title mb-3">
+                <strong>Irene y Humahuaca</strong>
+              </h5>
+              <p className="card-text textE text-dark">
+                “Ayyyy Laurita muero de amor con cada una de las fotos! No puedo
+                sacarme la sonrisa de la boca! Te juro que captaste a la hum que
+                es ella en acda una de las fotos! Y es para siempre! Ayy me voy
+                a abrazarla!!! Bello bello bello todo!!!
+              </p>
+              <span className="text-dark">
+                San Miguel de Tucumán - Argentina - 2020
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr className="hrp" />
+      <div className="cardE mb-3">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src={otti} className="img-fluid rounded-start" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body cardEx">
+              <h5 className="card-title  ">
+                <strong> Silvia y Otti</strong>
+              </h5>
+              <p className="card-text textE text-dark">
+                “Ay Lau recién termino de trabajar y estoy viendo las fotos,
+                unas ganas de llorar. Sus ojos en todas las fotos! lo amo tanto!
+                Gracias!!”
+              </p>
+              <span className="text-dark">
+                San Miguel de Tucumán - Argentina - 2022
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr className="hrp" />
     </>
   );
-  };
+};
 
-
-export default PreguntasFrecuentes
+export default PreguntasFrecuentes;

85ee6a0d5a7b3275c642294e84ae3ee649c69f62
Laura <bjtamyl@hotmail.com>
2022-10-18 18:29:23 -0300

    maquetando paquetes

--- src/components/Paquetes.jsx
+++ src/components/Paquetes.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import slider2 from "../assets/slider2.jpg";
+import rash from "../assets/rash.jpg";
+import paquetes from "../css/paquetes.css"
 
 const Paquetes = () => {
   return (
-    <div className="card text-bg-dark">
-      <img src={slider2} className="card-img" alt="..." />
+    <div className="card">
+      <div className="card-body">
+
+      <img src={rash} className="card-img" alt="..." />
       <div className="card-img-overlay">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text text-white">
+        <h5 className="card-title tituloPaq">Card title</h5>
+        <p className="card-text text-dark textoPaq position-absolute">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat odit
           quas quam sequi, perferendis eveniet dolorem quidem exercitationem
           odio neque blanditiis soluta totam natus expedita tempore? Sapiente
@@ -30,41 +33,10 @@ const Paquetes = () => {
           iure recusandae? Numquam rem eos voluptates, maiores expedita
           laudantium nostrum atque ipsum quisquam quaerat similique rerum
           placeat nemo nihil est aperiam reiciendis, distinctio vitae tempore,
-          eum necessitatibus optio facilis maxime eveniet. Eum aliquam deleniti,
-          blanditiis praesentium, porro possimus voluptates modi necessitatibus
-          ab mollitia suscipit recusandae a corporis fugit, doloribus provident.
-          Libero veniam dolorum enim harum labore maiores cupiditate, doloremque
-          nisi cum quae ab sint alias est aliquid possimus asperiores rem
-          dolorem quasi deserunt quam porro. Repellendus, repellat. Iure, quae?
-          Odio aperiam voluptas corporis magni animi labore aliquam fuga velit
-          eius consequuntur iure esse laboriosam corrupti accusamus vitae earum
-          nisi rerum, aut dolorem fugiat qui libero provident? Corporis vitae
-          fuga quos molestiae atque, exercitationem maxime, error expedita
-          doloremque temporibus ipsam? Delectus possimus sapiente dolorem
-          explicabo quas!
+       
         </p>
-        <p className="card-text text-white">
-          <small>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Blanditiis, eum! Quidem error nisi commodi cum est nam laudantium
-            quas. At, laudantium? Excepturi modi aperiam laudantium
-            reprehenderit obcaecati hic consequatur accusamus similique culpa
-            quidem tempora ipsa impedit ducimus, quia minus distinctio incidunt
-            beatae rem cupiditate quisquam nemo tenetur laboriosam? Dicta alias
-            earum quae voluptatibus ab eligendi velit tempore magnam sint animi
-            modi, rem sapiente minima reiciendis tenetur fugit omnis. Quidem
-            similique dignissimos exercitationem cumque nobis perspiciatis nisi
-            consequuntur porro facilis architecto ea, ad recusandae, cum
-            doloribus accusantium magni mollitia fugit tempore corrupti
-            repellendus ducimus asperiores iste ipsum! Recusandae aut nobis,
-            illo, ipsa nesciunt non atque odit iste voluptatibus et beatae vel
-            eveniet labore nihil quibusdam iusto sapiente quod quaerat
-            exercitationem incidunt officiis totam minima a. Atque voluptates
-            alias unde aspernatur ad et voluptate nemo fuga itaque optio, vero
-            neque necessitatibus, dolores quaerat. Veritatis minima a voluptas
-            quam cum, adipisci distinctio consequatur.
-          </small>
-        </p>
+       
+      </div>
       </div>
     </div>
   );

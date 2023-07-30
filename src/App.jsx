89af6fb99bd03b1a05e339a4706f8e8a4802872d
Laura Ortiz <bjtamyl@hotmail.com>
2023-07-30 22:07:22 +0200

    deploy test 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import {  Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RoutesDos from "../src/routes/RoutesDos";
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
 import Registro from "./pages/Registro";
 import Gallery from "./pages/Gallery";
 import Paquetes from "./pages/Paquetes";
@@ -9,7 +9,7 @@ import ProtectedRoutes from "./routes/ProtectedRoutes";
 
 function App() {
   return (
-  
+    <BrowserRouter>
       <Routes>
         <Route
           path="/*"
@@ -20,11 +20,12 @@ function App() {
           }
         />
 
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
         <Route path="/registro" element={<Registro />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/paquetes" element={<Paquetes />} />
       </Routes>
+      </BrowserRouter>
   
   );
 }
--- src/routes/RoutesDos.jsx
+++ src/routes/RoutesDos.jsx
@@ -5,7 +5,7 @@ import Home from "../pages/Home";
 import Paquetes from "../pages/Paquetes";
 import NotFound from "../pages/NotFound";
 import Gallery from "../pages/Gallery";
-import Login from "../pages/Login";
+// import Login from "../pages/Login";
 import Registro from "../pages/Registro";
 
 const RoutesDos = () => {
@@ -16,7 +16,7 @@ const RoutesDos = () => {
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/paquetes" element={<Paquetes />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
         <Route path="/registro" element={<Registro />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
